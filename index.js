08eb708d7a62984130d91505a277c4ce99dae34e
VihagaYohan <vihagayohan94@gmail.com>
2022-09-14 22:05:26 +0530

    Added - Extra service endpoint

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const colors = require('colors')
 const ErrorHandler = require('./middleware/Error')
 const cors = require('cors')
 
-const corsOptions = {
+/* const corsOptions = {
     origin: 'http://locatlhost:5000',
     credentials: true, //access-control-allow-credentials:true
     optionSuccessStatus: 200,
 }
-
+ */
 // import DB connection
 const connectDB = require('./config/db')
 
@@ -18,9 +18,10 @@ const auth = require('./routes/Authentication')
 const serviceCategory = require('./routes/Services')
 const order = require('./routes/Orders')
 const users= require('./routes/Users')
+const extraService = require('./routes/ExtraService')
 
 const app = express();
-app.use(cors())
+//app.use(cors())
 app.use(ErrorHandler)
 
 app.use(express.json())
@@ -37,6 +38,7 @@ app.use('/api/auth/', auth)
 app.use('/api/services/',serviceCategory)
 app.use('/api/orders/',order)
 app.use('/api/users/',users)
+app.use('/api/extraServices/',extraService)
 
 
 const PORT = 5000 || process.env.PORT;
@@ -50,5 +52,5 @@ process.on("unhandledRejection", (error, promise) => {
     console.log(`Error : ${error.message}`.red.bold);
 
     // close server and exit process
-    server.close(() => process.exit(1));
+    //server.close(() => process.exit(1));
 });
